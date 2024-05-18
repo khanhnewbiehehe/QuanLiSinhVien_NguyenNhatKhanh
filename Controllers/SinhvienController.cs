d221902c0ef92aa4b1e0c03f6d21cf43d224e611
khanhnewbiehehe <nguyennhatkhanh151203@gmail.com>
2024-05-19 01:17:24 +0700

    xem danh sách sinh viên giao diện mặc định

---
+++ Controllers/SinhvienController.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using QuanLiSinhVien.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace QuanLiSinhVien.Controllers
+{
+    public class SinhvienController : Controller
+    {
+        // GET: SinhVienController
+        public ActionResult Index()
+        {
+            var listSV = new QlsinhvienContext().Sinhviens.ToList();
+            return View(listSV);
+        }
+
+        // GET: SinhVienController/Details/5
+        public ActionResult Details(int id)
+        {
+           
+            return View();
+        }
+
+        // GET: SinhVienController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: SinhVienController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: SinhVienController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: SinhVienController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: SinhVienController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: SinhVienController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
