0fbe1a5962f85167bdbf82cd543825e915e12a49
khanhnewbiehehe <nguyennhatkhanh151203@gmail.com>
2024-05-19 18:28:53 +0700

    thêm giao diện tìm kiếm cho trang index lớp sinh hoạt

--- obj/Debug/net8.0/QuanLiSinhVien.AssemblyInfo.cs
+++ obj/Debug/net8.0/QuanLiSinhVien.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuanLiSinhVien")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+344a2bd310c5c9c78517a46b5cc0900f71457c6d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a939fc80d9a524f743c7a505d1b4c9a42f6c460")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuanLiSinhVien")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuanLiSinhVien")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
