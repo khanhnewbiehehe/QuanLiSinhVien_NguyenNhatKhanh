574fac8027be9bf222b1950d62562d86535219fe
khanhnewbiehehe <nguyennhatkhanh151203@gmail.com>
2024-05-22 12:01:12 +0700

    sửa lại giao diện theo vai trò

--- obj/Debug/net8.0/QuanLiSinhVien.AssemblyInfo.cs
+++ obj/Debug/net8.0/QuanLiSinhVien.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuanLiSinhVien")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26d983719e993bfff195b7587c02078ff0d4284f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd3c107f95925b3f51de90cf5a7e86d2d0914240")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuanLiSinhVien")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuanLiSinhVien")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
